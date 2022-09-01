b9250bbcbe76dda9ce1fe3b788c5208b622f01c8
DAG-CSTJ <2075312@cstj.qc.ca>
2022-09-01 15:00:08 -0400

    fin cours 2022-09-01

--- src/apps.js
+++ src/apps.js
@@ -1,13 +1,65 @@
+import dayjs from 'dayjs';
 import express from 'express'; // serveur http déjà fait pour nous
+
 
 const app = express();
 
 //TODO : Ajouter du code ici
 
-app.get('/premiere',(req,res)=>{
+app.get('/deuxieme',(req,res)=>{
 res.status(200)
 res.set('Content-Type','text/plain');
 res.send('Première route avec express');
 })
+
+app.get('/date', (req,res)=>{
+    res.status(200);
+    res.set('Content-Type', 'text/plain');
+    const today = dayjs();
+    res.send(today.format());
+
+
+});
+
+
+app.get('/matematik/:operation', (req,res)=>{
+     
+     const a = parseInt(req.query.a,10);
+     const b = parseInt(req.query.b,10);
+
+     const operation = req.params.operation;
+    let ok = 0;
+
+    switch (operation) {
+        case "somme":
+            
+            ok = a+b;
+
+        break;
+    
+        case 'différence':
+
+            ok = a-b;
+        break;
+
+        case "quotient":
+
+        ok = a*b;
+        
+        break;
+
+        case "produit":
+        ok = a/b;
+        break;
+        default:
+            result='Operation non définie'
+    }
+
+  //  const somme = (a+b).toString()
+    res.status(200);
+    res.set('Content-Type', 'text/plain');
+    res.send(ok.toString());    
+})
+
 
 export default app;
