c6bb92fb3b0d0339300be3c76636887c09b2bff7
DAG-CSTJ <2075312@cstj.qc.ca>
2022-09-20 09:34:56 -0400

    Create et transform pour TP1

--- src/repositories/planets.repositories.js
+++ src/repositories/planets.repositories.js
@@ -1,4 +1,5 @@
 import Planet from '../models/planet-model.js'
+import dayjs from 'dayjs';
 
 class PlanetsRepository {
 
@@ -6,7 +7,24 @@ class PlanetsRepository {
         return Planet.find(); // select * from planets
     }
 
+    retrieveOne(idPlanet){
+        return Planet.findById(idPlanet) // SELECT * FROM planets WHERE idPlanet = idPlanet
+    }
 
+    transform(planet){
+
+        planet.discoveryDate=dayjs(planet.discoveryDate).format('YYYY-MM-DD');
+
+        delete planet.createdAt;
+        delete planet.updateAt;
+        delete planet.__v;
+
+         return planet;
+    }
+
+    create(planet){
+       return Planet.create(planet); // INSERT() INTO planets VALUES()
+    }
 }
 
 
